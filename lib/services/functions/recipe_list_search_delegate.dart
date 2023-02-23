553d476ac629de5768e99cccabd3c3acc499b8be
Chris-Laptop <crecio12@gmail.com>
2023-02-23 17:09:58 -0500

    Recipe List search now functional

--- lib/services/functions/recipe_list_search_delegate.dart
+++ lib/services/functions/recipe_list_search_delegate.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:recipe_app/services/functions/recipe_provider.dart';
 
@@ -5,26 +7,45 @@ import '../../pages/view_recipe.dart';
 import '../models/recipe.dart';
 
 class RecipeListSearchDelegate extends SearchDelegate {
-  List<Map<String, dynamic>> _searchTerms = [];
+  List<Map<String, dynamic>> rawSearchResults = [];
+  List<String> searchResults = [];
+  List<Map<String, dynamic>> recipesByName = [];
 
   void _refreshRecipeList() async {
-    _searchTerms = await RecipeProvider.getAllRecipes();
-  }
-  // ^^^ returns [{name: 12}, {name: asd}, {name: gfd}]
+    if (rawSearchResults.isEmpty) {
+      rawSearchResults = await RecipeProvider.getAllRecipes();
 
+      for (var element in rawSearchResults) {
+        searchResults.add(element['name']);
+      }
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> _searchRecipeByName(String name) async {
+    final data = await RecipeProvider.searchRecipeByName(name);
+    recipesByName = data;
+    return recipesByName;
+  }
+
+  // right X button on search bar
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
       IconButton(
         onPressed: () {
-          query = '';
+          if (query.isEmpty) {
+            close(context, null);
+          } else {
+            query = '';
+            showResults(context);
+          }
         },
         icon: const Icon(Icons.clear),
       ),
     ];
   }
 
-  // second overwrite to pop out of search menu
+  // left back arrow
   @override
   Widget? buildLeading(BuildContext context) {
     return IconButton(
@@ -35,120 +56,88 @@ class RecipeListSearchDelegate extends SearchDelegate {
     );
   }
 
-  // third overwrite to show query result
+  // query results
   @override
   Widget buildResults(BuildContext context) {
-    List<Map<String, dynamic>> matchQuery = [];
-    for (var recipeName in _searchTerms) {
-      if (recipeName['name'].toLowerCase().contains(query.toLowerCase())) {
-        matchQuery.add(recipeName);
-      }
-    }
-    return GridView.builder(
-        padding: const EdgeInsets.all(10),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 10.0,
-          mainAxisSpacing: 10.0,
-        ),
-        itemCount: matchQuery.length,
-        itemBuilder: (BuildContext ctx, index) {
-          return InkWell(
-            child: Container(
-              decoration: const BoxDecoration(color: Colors.grey, borderRadius: BorderRadius.all(Radius.circular(10))),
-              padding: const EdgeInsets.all(5.0),
-              child: Center(
-                  child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  ClipRRect(
-                    borderRadius: const BorderRadius.all(Radius.circular(10)),
-                    child: matchQuery[index]['image'] != null
-                        ? Image.asset(
-                            'assets/images/logo.jpg',
-                            fit: BoxFit.cover,
-                            height: 140,
-                          )
-                        : Image.asset(
-                            'assets/images/logo.jpg',
-                            fit: BoxFit.cover,
-                            height: 140,
-                          ),
+    // recipesByName = await _searchRecipeByName(query);
+    // print(recipesByName);
+
+    return FutureBuilder(
+      future: _searchRecipeByName(query),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return GridView.builder(
+              padding: const EdgeInsets.all(10),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10.0,
+                mainAxisSpacing: 10.0,
+              ),
+              itemCount: recipesByName.length,
+              itemBuilder: (BuildContext ctx, index) {
+                return InkWell(
+                  child: Container(
+                    decoration: const BoxDecoration(color: Colors.grey, borderRadius: BorderRadius.all(Radius.circular(10))),
+                    padding: const EdgeInsets.all(5.0),
+                    child: Center(
+                        child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        ClipRRect(
+                          borderRadius: const BorderRadius.all(Radius.circular(10)),
+                          child: recipesByName[index]['image'] != null
+                              ? Image.file(
+                                  File(recipesByName[index]['image']),
+                                  fit: BoxFit.cover,
+                                  height: 140,
+                                )
+                              : Image.asset(
+                                  'assets/images/logo.jpg',
+                                  fit: BoxFit.cover,
+                                  height: 140,
+                                ),
+                        ),
+                        Text(
+                          recipesByName[index]['name'],
+                          style: const TextStyle(fontSize: 20.0),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    )),
                   ),
-                  Text(
-                    matchQuery[index]['name'],
-                    style: const TextStyle(fontSize: 20.0),
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              )),
-            ),
-            onTap: () => {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ViewRecipe(recipe: Recipe.fromMap(matchQuery[index])),
-                  ))
-            },
-          );
-        });
+                  onTap: () => {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ViewRecipe(recipe: Recipe.fromMap(recipesByName[index])),
+                        ))
+                  },
+                );
+              });
+        } else if (snapshot.hasError) {
+          return const Text("Error");
+        }
+        return const Text("Loading...");
+      },
+    );
   }
 
-  // last overwrite to show the
-  // querying process at the runtime
+  // query suggestions
   @override
   Widget buildSuggestions(BuildContext context) {
-    List<Map<String, dynamic>> matchQuery = [];
-    for (var recipeName in _searchTerms) {
-      if (recipeName['name'].toLowerCase().contains(query.toLowerCase())) {
-        matchQuery.add(recipeName);
-      }
-    }
-    return GridView.builder(
-        padding: const EdgeInsets.all(10),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 10.0,
-          mainAxisSpacing: 10.0,
-        ),
-        itemCount: matchQuery.length,
-        itemBuilder: (BuildContext ctx, index) {
-          return InkWell(
-            child: Container(
-              decoration: const BoxDecoration(color: Colors.grey, borderRadius: BorderRadius.all(Radius.circular(10))),
-              padding: const EdgeInsets.all(5.0),
-              child: Center(
-                  child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  ClipRRect(
-                    borderRadius: const BorderRadius.all(Radius.circular(10)),
-                    child: matchQuery[index]['image'] != null
-                        ? Image.asset(
-                            'assets/images/logo.jpg',
-                            fit: BoxFit.cover,
-                            height: 140,
-                          )
-                        : Image.asset(
-                            'assets/images/logo.jpg',
-                            fit: BoxFit.cover,
-                            height: 140,
-                          ),
-                  ),
-                  Text(
-                    matchQuery[index]['name'],
-                    style: const TextStyle(fontSize: 20.0),
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              )),
-            ),
-            onTap: () => {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ViewRecipe(recipe: Recipe.fromMap(matchQuery[index])),
-                  ))
+    _refreshRecipeList();
+    List<String> suggestions = searchResults.where((i) => i.toLowerCase().contains(query.toLowerCase())).toList();
+
+    return ListView.builder(
+        itemCount: suggestions.length,
+        itemBuilder: (context, index) {
+          final suggestion = suggestions[index];
+
+          return ListTile(
+            title: Text(suggestion),
+            onTap: () {
+              query = suggestion;
+              showResults(context);
             },
           );
         });
